e8d3685919b4eaae3c1305847f8b368b0b481a2b
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-02 10:09:55 +0100

    import error and custom errors handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { psqlErrors, customErrors, handleFiveHundreds } = require('./errors.js')
 const { getTopics } = require('./controllers/topics.controllers')
 
 const express = require('express')
@@ -7,5 +8,14 @@ const app = express()
 app.use(express.json())
 
 app.get('/api/topics', getTopics)
+
+// errors and custom errors
+
+// app.all('*', (req, res) => {
+//   res.status(404).send({ msg: 'Path not found' })
+// })
+app.use(psqlErrors)
+app.use(customErrors)
+app.use(handleFiveHundreds)
 
 module.exports = app
