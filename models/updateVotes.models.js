37cf3d0e4566b27bdff416b6cdafc91801d1faea
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-03 10:38:32 +0100

    refactor some code, complete updateVotes function

--- models/updateVotes.models.js
+++ models/updateVotes.models.js
@@ -1,11 +1,12 @@
 const db = require('../db/connection')
 
 exports.updateVotes = async (article_id, votes) => {
-  const {
-    rows: [items],
-  } = await db.query(
+  const { rows } = await db.query(
     `UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *`,
     [article_id, votes]
   )
-  return items
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: 'Article not found' })
+  }
+  return rows[0]
 }
