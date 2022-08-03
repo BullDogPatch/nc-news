b71b18525215a81d8743f2bd9843a53cda976a6f
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-03 15:44:15 +0100

    remove console.log and comment

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,6 @@ const db = require('../db/connection')
 exports.selectArticleById = article_id => {
   return db
     .query(
-      // couldn't get this to work with just count comments, so had to query the whole object
       `SELECT COUNT (comments.comment_id) AS comment_count, title, articles.article_id, articles.author, articles.body, topic, articles.created_at, articles.votes FROM articles
     LEFT JOIN comments
     ON articles.article_id = comments.article_id
