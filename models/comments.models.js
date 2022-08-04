19e6f9af1a6334f4b5467328f12e9c00cf2abff8
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-04 16:53:23 +0100

    reworked model

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,8 +1,19 @@
 const db = require('../db/connection')
 
-exports.fetchCommentsByArticleId = article_id => {
+exports.fetchComments = article_id => {
   return db
-    .query('SELECT * FROM comments where article_id = $1;', [article_id])
+    .query(
+      `SELECT  
+  comments.comment_id,
+  comments.votes, 
+  comments.created_at,
+  comments.author,
+  comments.body
+  FROM comments
+  INNER JOIN articles ON comments.article_id = articles.article_id
+  WHERE articles.article_id = $1; `,
+      [article_id]
+    )
     .then(({ rows: article }) => {
       if (article.length === 0) {
         return Promise.reject({
