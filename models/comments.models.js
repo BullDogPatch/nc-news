badee2a96c0e3abdbddcc62b53067edf649b128f
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-04 20:01:20 +0100

    made the functions async for readabilty

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,30 +1,5 @@
 const db = require('../db/connection')
 
-// exports.fetchComments = article_id => {
-//   return db
-//     .query(
-//       `SELECT
-//   comments.comment_id,
-//   comments.votes,
-//   comments.created_at,
-//   comments.author,
-//   comments.body
-//   FROM comments
-//   INNER JOIN articles ON comments.article_id = articles.article_id
-//   WHERE articles.article_id = $1; `,
-//       [article_id]
-//     )
-//     .then(({ rows: article }) => {
-//       if (article.length === 0) {
-//         return Promise.reject({
-//           status: 404,
-//           msg: `Article not found`,
-//         })
-//       }
-//       return article
-//     })
-// }
-
 exports.fetchComments = article_id => {
   return db
     .query(
