6be6a246ad557edf4a5a60862deaf29697c6eec3
bulldogpatch <mancityvsliverpoolfc@outlook.com>
2022-08-04 19:59:11 +0100

    add commentCount function to tidy code up

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,23 +1,18 @@
 const db = require('../db/connection')
+const { commentCount } = require('./commentCount')
 
-exports.fetchAllArticles = () => {
-  return db
-    .query(
-      'SELECT articles.*, COUNT (comments.article_id) :: INTEGER AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id  GROUP BY articles.article_id ORDER BY created_at DESC'
-    )
-    .then(({ rows }) => {
-      return rows
-    })
+exports.fetchAllArticles = async () => {
+  const articles = await db.query(
+    `SELECT articles.*, COUNT (comments.article_id) :: INTEGER AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id  GROUP BY articles.article_id ORDER BY created_at DESC`
+  )
+  const { rows } = articles
+  return rows
 }
 
 exports.selectArticleById = article_id => {
   return db
     .query(
-      `SELECT COUNT (comments.comment_id) AS comment_count, title, articles.article_id, articles.author, articles.body, topic, articles.created_at, articles.votes FROM articles
-    LEFT JOIN comments
-    ON articles.article_id = comments.article_id
-    WHERE articles.article_id = $1
-    GROUP BY articles.article_id`,
+      commentCount(),
 
       [article_id]
     )
